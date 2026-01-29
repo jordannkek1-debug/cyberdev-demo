8c0e5ddf456996976505f7013f5c9a4c7b136356
Nesi05 <46183636+Nesi05@users.noreply.github.com>
2026-01-29 12:49:26 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+# Voici notre premier project
+# nous voulons un code python qui permet a l'utilisateur de deviner un nombre:
+# Entrer un nombre, et quand l'utilisateur entre le nombre, ça dit si le nombre entré est grand ou petit. ou il a trouver le bon nombre
